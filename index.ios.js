f53ae860049a3495027a6b43e535fbe0deb4d14d
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-05-27 20:41:48 -0500

    Funciona cargar imagen

--- index.ios.js
+++ index.ios.js
@@ -4,26 +4,82 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
+  ImagePickerIOS,
+  Image,
+  CameraRoll,
 } from 'react-native';
 import Camera from 'react-native-camera';
 
-export default class Clothy extends Component {
-  constructir(){
-    this.state = {ventana: 'camara'}
+var ImagePicker = require('react-native-image-picker');
+
+
+var options = {
+  title: 'Upload Photo',
+  customButtons: [],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
   }
+};
+
+
+export default class Clothy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ventana: 'camara', fuente: { uri: "http://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg" }, image: '' }
+  }
+
   takePicture() {
     this.camera.capture()
-      .then((data) => console.log(data))
+      .then((data) => (this.setState({ fuente: data.path })))
       .catch(err => console.error(err));
   }
+
+  uploadPicture() {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else {
+        let source64 = { uri: 'data:image/jpeg;base64,' + response.data };
+        let source = { uri: response.uri };
+
+        this.setState({ fuente: source });
+
+        fetch('http://clothy-dev.us-east-1.elasticbeanstalk.com/recommend', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            img: "'" + source + "'"
+          })
+        })
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+      }
+    });
+
+  }
+
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Clothy
         </Text>
-
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={this.state.fuente}
+        />
         <Camera
           ref={(cam) => {
             this.camera = cam;
@@ -31,6 +87,7 @@ export default class Clothy extends Component {
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}>
           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          <Text style={styles.capture} onPress={this.uploadPicture.bind(this)}>[UPLOAD]</Text>
         </Camera>
       </View>
     );
